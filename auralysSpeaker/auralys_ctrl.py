a770b0a3edebda6148793a16b19dffd00d67d32a
Gianluca Filippini <gianluca.filippini@ebv.com>
2025-06-06 18:12:23 +0200

    changes for command selection

--- auralysSpeaker/auralys_ctrl.py
+++ auralysSpeaker/auralys_ctrl.py
@@ -6,9 +6,12 @@ import logging
 import sys
 import asyncio
 import yaml
-import wget
+#import wget
+import requests
+import multiprocessing
 import json
 import subprocess
+import multiprocess
 import time
 import math
 import os
@@ -36,38 +39,54 @@ logger = logging.getLogger(__name__)
 #          (R)
 
 # left stand position and size
-hL_mm=2500.0
-dL_mm=600.0
+hL_mm=2600.0
+dL_mm=550.0
 maxL=600000.0
-minL=600000.0
-mks_L_step_mm = float(100000.0/145.0)
+minL=-600000.0
+mks_L_step_mm = float(1000000.0/1480.0)
 
 # right stand position and size
-hR_mm=2500.0
-dR_mm=600.0
+hR_mm=2600.0
+dR_mm=550.0
 maxR=600000.0
-minR=600000.0
-mks_R_step_mm = float(100000.0/140.0)
+minR=-600000.0
+mks_R_step_mm = float(1000000.0/1600.0)
 
 # front stand position and size
-hF_mm=2500.0
-dF_mm=1700.0
+hF_mm=2600.0
+dF_mm=1540.0
 maxF=600000.0
-minF=600000.0
-mks_F_step_mm = float(100000.0/145.0)
+minF=-600000.0
+mks_F_step_mm = float(1000000.0/1505.0)
 
-mks_origin_x = 1050.0
-mks_origin_y = 0.0
-mks_origin_z = 1450.0
+# origin (zero point) for MKS steppers
+mks_origin_x_mm = 700.0
+mks_origin_y_mm = 0.0
+mks_origin_z_mm = 1520.0
 
+# origin (zero point) for head centroid
+head_origin_x_mm = 0
+head_origin_y_mm = 0
+head_origin_z_mm = 1680
+
+# ip_addresses of the auralys units
+ip_addr_L="192.168.10.32" # left stand
+ip_addr_R="192.168.10.34" # right stand
+ip_addr_F="192.168.10.173" # center stand
+ip_addr_S="192.168.10.100" # speaker mount
 
 #
+# ##################################################################################### #
 # DO NOT CHANGE DEFINES BELOW THIS LINE
+# ##################################################################################### #
 # 
 
 _ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 _CFG_FILENAME = "auralys_ctrl_cfg.json"
-_PARAM_LIST=["position", "speed", "accel", "zero"]
+_SET_PARAM_LIST=["position", "speed", "accel"]
+_GET_PARAM_LIST=["position", "speed", "accel"]
+_COORD_TYPE_DICT={"ac":"absolute, cartesian","as":"absolute, spherical","rc":"relative, cartesian","rs":"relative, spherical"}
+_CMD_LIST=["gozero","setzero","stop"]
 
 #
 # Tools
@@ -80,7 +99,7 @@ def int_or_str(text):
     except ValueError:
         return text
 
-def compute_cables_length(x,y,z):
+def compute_wires_length(x,y,z):
     tmpF =math.sqrt((dF_mm-x)**2 + y**2)
     tmpL =math.sqrt((dL_mm-y)**2 + x**2)
     tmpR =math.sqrt((dR_mm+y)**2 + x**2)
@@ -89,76 +108,190 @@ def compute_cables_length(x,y,z):
     lengthL=math.sqrt(tmpL**2 + (hL_mm-z)**2)
     lengthR=math.sqrt(tmpR**2 + (hR_mm-z)**2)
 
-    print(lengthL)
-    print(lengthR)
-    print(lengthF)
-
     return [lengthL,lengthR, lengthF]
+
+def verify_coord_limits(x,y,z):
+    # SANITY CHECK: verify coord outside of limits
+    if ( (abs(y)>=dL_mm) or (abs(y)>=dR_mm) or (x<0) or (abs(x)>=dF_mm) or (z<0) or (abs(z)>=(min([hF_mm,hR_mm,hL_mm]))) ):
+        logger.error("Invalid coordinate value")
+        return -1
+
+    # SANITY CHECK: verify coord inside the triangle
+    angleR_ref=math.atan(dF_mm/dR_mm)
+    angleL_ref=math.atan(dF_mm/dL_mm)
+    if(angleR_ref < math.atan(x/(dR_mm+y))) or (angleL_ref < math.atan(x/(dL_mm-y))):
+        logger.error("Coordinate outside of boundaries")
+        return -1
+
+    return 0
+
+def mks_set_length( ip_addr,mks_length):
+    url = "http://"+str(ip_addr)+"/position/set/"
+    # print(url)
+    # print(str(int(mks_length)))
+    response = requests.post(url, data=str(int(mks_length)))
+    #print("mks_set_length, Response: "+response.text)
+    result = json.loads(response.text)
+    if(result["error"] !=0 ):
+        return -1
+
+    return 0
+
+def mks_get_status( ip_addr, option):
+    url = "http://"+str(ip_addr)+"/status/get/"
+    response = requests.get(url)
+
+    result = json.loads(response.text)
+    if(result["error"] !=0 ):
+        return -1
+
+    return result["status"]
+
+def mks_set_positions(mks_position_F,mks_position_L,mks_position_R):
+    rv = 0
+
+    logger.info("mks_set_position, mks_F: "+str(mks_position_F))
+    logger.info("mks_set_position, mks_L: "+str(mks_position_L))
+    logger.info("mks_set_position, mks_R: "+str(mks_position_R))
+
+    # STEP #0: GET current position and verify status
+    task_args = [
+        (ip_addr_F, mks_position_F),
+        (ip_addr_L, mks_position_L),
+        (ip_addr_R, mks_position_R)
+    ]
+
+    with multiprocessing.Pool(processes=3) as pool:
+        results = pool.starmap(mks_get_status, task_args)
+
+    # check result for errors
+    err = 0
+    for result in results:
+        err += result
+    
+    if(err!=0):
+        logger.error("mks_set_position: error while executing GET POSITION")
+
+
+    # STEP #1: SET current position and verify status
+    task_args = [
+        (ip_addr_F, mks_position_F),
+        (ip_addr_L, mks_position_L),
+        (ip_addr_R, mks_position_R)
+    ]
+
+    with multiprocessing.Pool(processes=3) as pool:
+        results = pool.starmap(mks_set_length, task_args)
+
+    # check result for errors
+    err = 0
+    for result in results:
+        err += result
+    
+    if(err!=0):
+        logger.error("mks_set_position: error while executing SET POSITION")
+
+    print(err)
+
+    return rv
+
 
 #
 # SET Commands
 #
 def set_position(x,y,z,type):
-    print("X="+str(x))
-    print("y="+str(y))
-    print("z="+str(z))
-
+    rv = 0
     x=float(x)
     y=float(y)
     z=float(z)
+
     # COORD TYPE: absolute cartesian
-    if("ac"):
-        # SANITY CHECK: verify coord outside of limits
-        if ( (abs(y)>=dL_mm) or (abs(y)>=dR_mm) or (x<0) or (abs(x)>=dF_mm) or (z<0) or (abs(z)>=(min([hF_mm,hR_mm,hL_mm]))) ):
-            logger.error("Invalid coordinate value")
+    if("ac"==type):
+
+        # SANITY CHECK
+        if( 0!=verify_coord_limits(x,y,z)):
+            logger.error("coordinates verification failed.")
             return -1
 
-        # SANITY CHECK: verify coord inside the triangle
-        angleR_ref=math.atan(dF_mm/dR_mm)
-        angleL_ref=math.atan(dF_mm/dL_mm)
-        if(angleR_ref < math.atan(x/(dR_mm+y))) or (angleL_ref < math.atan(x/(dL_mm-y))):
-            logger.error("Coordinate outside of boundaries")
+        [mks_originL, mks_originR, mks_originF]=compute_wires_length(float(mks_origin_x_mm),float(mks_origin_y_mm),float(mks_origin_z_mm))
+
+        [lengthL, lengthR, lengthF]=compute_wires_length(x,y,z)
+
+        print("F: "+str(lengthF))
+        print("L: "+str(lengthL))
+        print("R: "+str(lengthR))
+
+
+        mks_position_F = ((mks_originF-lengthF)*mks_F_step_mm)
+        mks_position_L = ((mks_originL-lengthL)*mks_L_step_mm)
+        mks_position_R = ((mks_originR-lengthR)*mks_R_step_mm)
+
+        print(mks_position_F)
+        print(mks_position_L)
+        print(mks_position_R)
+
+        if( ((mks_position_F>maxF) or (mks_position_F<minF)) or ((mks_position_R>maxR) or (mks_position_R<minR)) or ((mks_position_L>maxL) or (mks_position_L<minL)) ):
+            logger.error("set_position: wire lengths outside of limits")
             return -1
 
-        [mks_originL, mks_originR, mks_originF]=compute_cables_length(float(mks_origin_x),float(mks_origin_y),float(mks_origin_z))
+        rv=mks_set_positions(mks_position_F,mks_position_L,mks_position_R)
 
-        [lengthL, lengthR, lengthF]=compute_cables_length(x,y,z)
-
-        # COMPUTE CABLEs LENGTH
-        print("ORIGINS:")
-        print(mks_originF)
-        print(mks_originL)
-        print(mks_originR)
-
-        print("RESULTS:")
-        print(lengthF)
-        print(lengthL)
-        print(lengthR)
-
-        print("RESULTS:")
-        print((mks_originF-lengthF)*mks_F_step_mm)
-        print((mks_originL-lengthL)*mks_L_step_mm)
-        print((mks_originR-lengthR)*mks_R_step_mm)
-
+    elif("rs"==type):
+        print("relative spherical")
+ 
     else:
-        logger.error("Unsupported coordintate type: "+type)
+        if(type in _COORD_TYPE_DICT):
+            logger.error("Not implemented coordintate type, "+type+" ["+_COORD_TYPE_DICT[type]+"]")
+        else:
+            logger.error("Unsupported coordintate type: "+type)
         return -1
 
-    return (0)
+    return (rv)
 
 def set_select(args):
     rv=0
 
-    if("position"==args.param[0]):
+    if("position"==args.command[1]):
         try:
-            [x,y,z]=list(args.param[1].strip().split(","))
+            [x,y,z]=list(args.param[0].strip().split(","))
         except:
             logger.error("Invalid coordinate format, should comma separated: x,y,z")
             return (-1)
-
         rv=set_position(x,y,z,args.type)
+    else:
+        logger.error("unsupported SET option")
+        rv=-1
+
+    return (rv)
+
+#
+# GET Commands
+#
+
+def get_select(args):
+    rv=0
+
+    logger.error("GET not yet implemented. exit.")    
+
+    return (rv)
+
+
+#
+# CMD Commands
+#
+
+def cmd_select(args):
+    rv=0
+
+    if("gozero"==args.command[1]):
+            rv=set_position(mks_origin_x_mm,mks_origin_y_mm,mks_origin_z_mm,"ac")
+    else:
+        logger.error("unsupported CMD option")
+        rv=-1
     
     return (rv)
+
+    
 
 
 #
@@ -170,6 +303,7 @@ if __name__ == "__main__":
         "-c",
         "--command",
         type=str,
+        nargs=2,
         default=None,
         help="set/get/cmd command (default: %(default)s)",
     )
@@ -178,8 +312,8 @@ if __name__ == "__main__":
         "-p",
         "--param",
         type=str,
-        nargs=2,
-        default=[None,None],
+        nargs=1,
+        default=None,
         help="param name (default: %(default)s)",
     )
 
@@ -291,15 +425,15 @@ if __name__ == "__main__":
         logger.error("Command missing.")
         exit(0)
 
-    if(args.command=="set"):
+    if(args.command[0]=="set"):
         set_select(args)
-    elif (args.command=="get"):
+    elif (args.command[0]=="get"):
         get_select(args)
-    elif (args.command=="cmd"):
+    elif (args.command[0]=="cmd"):
         cmd_select(args)
     else:
         logger.error("Command invalid/unsupported.")
         exit(0)
 
 
-    print("ciao.")
+    logger.info("done.")
