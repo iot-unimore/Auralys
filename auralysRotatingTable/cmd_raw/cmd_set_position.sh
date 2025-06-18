84876d3819b2e01123eb681a243037c6a9343ba4
Gianluca Filippini <gianluca@autosburla.com>
2025-06-18 19:07:05 +0200

    fix raw cms script for rotating table

--- auralysRotatingTable/cmd_raw/cmd_set_position.sh
+++ auralysRotatingTable/cmd_raw/cmd_set_position.sh
@@ -1,4 +1,30 @@
 
 args=("$@")
-echo wget -qO- --post-data ${args[0]} http://192.168.10.223/position/set
+
+verbose=1
+
+for i in $args; do
+  case $i in
+    -q|--quiet)
+      verbose=0
+      shift # past argument=value
+      ;;
+    --default)
+      DEFAULT=YES
+      shift # past argument with no value
+      ;;
+    -*|--*)
+      echo "Unknown option $i"
+      exit 1
+      ;;
+    *)
+      ;;
+  esac
+done
+
+if [[ $verbose -gt 0 ]]; then
+   echo wget -qO- --post-data ${args[0]} http://192.168.10.223/position/set
+   echo $verbose
+fi
+
 wget -qO- --post-data ${args[0]} http://192.168.10.223/position/set
