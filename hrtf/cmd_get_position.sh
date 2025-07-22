29ce7bf7e22744c312356e732999483b7edf7334
Gianluca Filippini <gianluca@autosburla.com>
2025-07-22 14:24:27 +0200

    fix get_position regexp

--- hrtf/cmd_get_position.sh
+++ hrtf/cmd_get_position.sh
@@ -11,6 +11,8 @@ angle=$(echo "($mks_steps * (360.0/2454840))"  | tr -d '\r' | bc -l)
 
 angle_int=$(echo "$angle" | awk '{print int($1+0.5)}')
 
-output_yaml=$(echo "$output" | sed -E "s/\"encoder\"[[:space:]]*:[[:space:]]*[0-9]+/\"position\": $angle_int/")
+echo "$output"
+
+output_yaml=$(echo "$output" | sed -E "s/\"encoder\"[[:space:]]*:[[:space:]]*-?[0-9]+/\"position\": $angle_int/")
 
 echo "$output_yaml"
